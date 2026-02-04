9a89789c38c0e4d186f3a59fa90250fc74eb970a
Madhav Beri <madhavber222@gmail.com>
2026-02-05 02:09:37 +0530

    UI Polish: White theme, Responsive Grid, Custom Favicon, and Login Prompt

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,19 @@ function Home() {
         })
     }, [])
     if (posts.length === 0) {
-        return <div className='py-8'>No posts found</div>
+        return (
+            <div className="w-full py-8 mt-4 text-center">
+                <Container>
+                    <div className="flex flex-wrap">
+                        <div className="p-2 w-full">
+                            <h1 className="text-2xl font-bold hover:text-gray-500">
+                                Login to read posts
+                            </h1>
+                        </div>
+                    </div>
+                </Container>
+            </div>
+        )
     }
     return (
         <div className=' w-full py-8 '>
